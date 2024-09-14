b398a2069cd6d264ac95099a9dc4b81644e77590
khiemict0902 <khiem0902@gmail.com>
2024-09-14 14:26:52 +0700

    first init

---
+++ app/src/main/java/vn/edu/usth/wordpress_client_mobile_app/Account/AccountActivity.java
@@ -0,0 +1,19 @@
+package vn.edu.usth.wordpress_client_mobile_app.Account;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.usth.wordpress_client_mobile_app.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/vn/edu/usth/wordpress_client_mobile_app/Media/MediaActivity.java
@@ -0,0 +1,19 @@
+package vn.edu.usth.wordpress_client_mobile_app.Media;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.usth.wordpress_client_mobile_app.R;
+
+public class MediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
